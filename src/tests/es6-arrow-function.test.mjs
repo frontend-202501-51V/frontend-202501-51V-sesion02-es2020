475ba16a02b2128537b4b67e29a3c982e22206fa
Roy Surjano <pxrsurja@cibertec.edu.pe>
2025-05-09 21:46:05 -0500

    subiendo test de arrow function

---
+++ src/tests/es6-arrow-function.test.mjs
@@ -0,0 +1,14 @@
+describe("validando compatibilidad de function y arrow function", () => {
+
+  it("debe arrow y function retornar el mismo resultado", () => {
+    // declara una funcion, que retorne la resta de dos numeros
+    // const myFn =
+
+    // declara un arrow function que retorne la resta de dos numeros
+    // const myArrowFn =
+
+    expect(myFn(5,3)).toBe(myArrowFn(5,3)) //
+
+  })
+
+})
